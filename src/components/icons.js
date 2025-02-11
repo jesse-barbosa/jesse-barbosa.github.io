d15c4b5ceef6d32a403c4927f13b8216e2e69a65
jesse-barbosa <barbosajesse419@gmail.com>
2025-02-11 08:24:58 -0300

    Removing language images, and using react-icons instead

--- src/components/icons.js
+++ src/components/icons.js
@@ -1,27 +1,38 @@
 import React from 'react';
+import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaNodeJs, FaPhp, FaDatabase } from 'react-icons/fa';
+import { SiTypescript, SiBootstrap } from 'react-icons/si';
 
 const iconData = [
-  { src: '/assets/languages/html.png', alt: 'HTML', class: 'ms-5 ps-3' },
-  { src: '/assets/languages/css.png', alt: 'CSS' },
-  { src: '/assets/languages/javascript.png', alt: 'JavaScript' },
-  { src: '/assets/languages/typescript.png', alt: 'Typescript' },
-  { src: '/assets/languages/bootstrap.png', alt: 'Bootstrap' },
-  { src: '/assets/languages/react.png', alt: 'React' },
-  { src: '/assets/languages/nodejs.png', alt: 'NodeJS' },
-  { src: '/assets/languages/php.png', alt: 'PHP' },
-  { src: '/assets/languages/sql.png', alt: 'SQL' },
+  { Icon: FaHtml5, alt: 'HTML', color: '#E34F26' },
+  { Icon: FaCss3Alt, alt: 'CSS', color: '#1572B6' },
+  { Icon: FaJs, alt: 'JavaScript', color: '#F7DF1E' },
+  { Icon: SiTypescript, alt: 'Typescript', color: '#3178C6' },
+  { Icon: SiBootstrap, alt: 'Bootstrap', color: '#9235FC' },
+  { Icon: FaReact, alt: 'React', color: '#61DAFB' },
+  { Icon: FaNodeJs, alt: 'NodeJS', color: '#339933' },
+  { Icon: FaPhp, alt: 'PHP', color: '#777BB4' },
+  { Icon: FaDatabase, alt: 'SQL', color: '#4479A1' },
 ];
 
 const repeatArray = (arr, times) => Array.from({ length: times }, () => arr).flat();
 
 const Icons = () => {
-  const repeatedIcons = repeatArray(iconData, 6);
+  const repeatedIcons = repeatArray(iconData, 3);
 
   return (
-    <div className="scrolling-content">
-      {repeatedIcons.map((icon, index) => (
-        <img key={index} src={icon.src} alt={icon.alt} className={icon.class} />
-      ))}
+    <div className="scrolling-content py-4">
+      {repeatedIcons.map((icon, index) => {
+        const IconComponent = icon.Icon;
+        return (
+          <IconComponent
+            key={index}
+            size={80}
+            color={icon.color}
+            title={icon.alt}
+            className="mx-4 pointer"
+          />
+        );
+      })}
     </div>
   );
 };
