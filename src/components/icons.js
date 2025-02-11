23d3b61c6095011964bf03abf58a1c847df152be
jesse-barbosa <barbosajesse419@gmail.com>
2025-02-11 08:33:52 -0300

    New dynamic item in iconsData array

--- src/components/icons.js
+++ src/components/icons.js
@@ -3,7 +3,7 @@ import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaNodeJs, FaPhp, FaLaravel, FaDataba
 import { SiTypescript, SiBootstrap, SiTailwindcss } from 'react-icons/si';
 
 const iconData = [
-  { Icon: FaHtml5, alt: 'HTML', color: '#E34F26' },
+  { Icon: FaHtml5, alt: 'HTML', class: 'ms-5 ps-3', color: '#E34F26' },
   { Icon: FaCss3Alt, alt: 'CSS', color: '#1572B6' },
   { Icon: FaJs, alt: 'JavaScript', color: '#F7DF1E' },
   { Icon: SiTypescript, alt: 'Typescript', color: '#3178C6' },
@@ -31,7 +31,7 @@ const Icons = () => {
             size={80}
             color={icon.color}
             title={icon.alt}
-            className="mx-4 pointer"
+            className={icon.class + ' mx-4'}
           />
         );
       })}
