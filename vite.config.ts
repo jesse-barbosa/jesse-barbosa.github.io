1ab6195e3f9ee2d0f06f9b343e01c690866ba4ce
jesse-barbosa <barbosajesse419@gmail.com>
2026-01-15 17:51:54 -0300

    fix: project build configuration in vite.config file

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,11 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-declare module "@remix-run/node" {
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
 export default defineConfig({
   base: "/",
   plugins: [
     remix({
+      ssr: false,
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
